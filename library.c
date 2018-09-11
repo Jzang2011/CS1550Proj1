90e7aed56691cc96d66c7680c1903683640909c5
Jeremy Zang <zang.jeremy@gmail.com>
2018-09-10 20:11:35 -0400

    added more untested  code to init_graphics()

--- library.c
+++ library.c
@@ -5,8 +5,61 @@
 // gcc -o driver driver.o library.o
 
 #include <time.h>
+#include <sys/select.h>
+#include <sys/mman.h>
+#include <sys/ioctl.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#define rgb(r,g,b) (((r & 31) << 11) + ((g & 63) << 5) + (b & 31))
+
+#include <stdio.h> //just for testing
+
+
+
+typedef unsigned short color_t;
 
 void init_graphics() {
+	//1. Open graphics device 
+		// using a framebuffer
+	char filePath[] = "/dev/fb0"; // path to framebuffer 
+	printf("%s", filePath);
+
+    int fD = 0; //fD = fildDescriptor
+    fD = open("/dev/fb0", O_RDWR);
+    if (fD == -1) {
+        perror("Error: cannot open framebuffer device");
+        exit(1);
+    }
+
+    //3  use typedef to make a color type color_t
+    // use ioctl to get screen size and bits per pixels
+    struct fb_var_screeninfo vInfo;
+    struct fb_fix_screeninfo fInfo;
+
+    int retVal = ioctl(fD, FBIOGET_FSCREENINFO, &fInfo);
+    int retVal2 = ioctl(fbfd, FBIOGET_VSCREENINFO, &vInfo);
+
+    int yRez = fb_var_screeninfo.yres_virtual;
+    int xRez = fb_fix_screeninfo.line_length;
+
+    int mmapLength = yRez * xRez;
+
+    //2. memory mapping mmap()
+    // void *mmap(void *addr, size_t length, int prot, int flags, int fd, off_t offset);
+    // length = yrez_virtual * line_length?
+    void* mmapAddress = mmap(NULL, mmapLength, PROT_READ | PROT_WRITE ,MAP_SHARED, fD, 0);
+
+    //4. clear screen
+    //Print "\033[2J" to tell the terminal to clear itself
+    printf(1, "\033[2J");
+
+
+    struct termios termSettings;
+	//5. Disable keypress echo.
+    int temp1 = ioctl(fD, TCGETS, &termSettings);
+    // disable canonical mode by unsetting the ICANON bit and disaling ECHO by forcing that bit to zero.
 
 }
 
@@ -15,7 +68,7 @@ void exit_graphics() {
 }
 
 char getkey() {
-	
+	int returnVale = select();
 }
 
 void sleep_ms(long ms) {
@@ -46,4 +99,9 @@ void *new_offscreen_buffer() {
 
 void blit(void *src) {
 	
+}
+
+int main() {
+	init_graphics();
+	return 0;
 }
